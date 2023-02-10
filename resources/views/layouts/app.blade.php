ee288804072d80e4c797c9d6f2066a4c7e26383f
Ali Hayder <ali.hayder.rzayyig@gmail.com>
2023-02-10 14:04:59 +0300

    update bg

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
 </head>
 
 <body class="font-sans antialiased">
-    <div class="min-h-screen bg-red-900">
+    <div class="min-h-screen bg-gray-700">
         @include('layouts.navigation')
 
         <!-- Page Heading -->
